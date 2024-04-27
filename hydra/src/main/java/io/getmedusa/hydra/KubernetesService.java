25a2f702aeb6ebc03ab38bb501e299ff4e82c07b
Kevin Deyne <kevindeyne@gmail.com>
2024-04-27 16:21:00 -0700

    Only printout when changed

--- hydra/src/main/java/io/getmedusa/hydra/KubernetesService.java
+++ hydra/src/main/java/io/getmedusa/hydra/KubernetesService.java
@@ -10,43 +10,40 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 @EnableScheduling
 public class KubernetesService {
 
     private final String uniqueID = UUID.randomUUID().toString();
+    Set<String> knownIPs = new LinkedHashSet<>();
 
-    @Scheduled(initialDelay = 5000, fixedDelay = 5000)
+    @Scheduled(initialDelay = 0, fixedDelay = 5000)
     public void scheduledK8sUpdate() {
-        System.out.println("K8S SETUP");
-        System.out.println("----");
-
         final String selfIP = System.getenv("SELF_POD_IP");
 
         final Set<String> instanceIPs = determinePodIPs();
 
-        //printing out ips
-        for(String ip : instanceIPs) {
-            String ipNote = ip;
-            if(selfIP.equals(ip)) {
-                ipNote += " (self)";
+        if(!instanceIPs.equals(knownIPs)) {
+            knownIPs = instanceIPs;
+            System.out.println("K8S SETUP");
+            System.out.println("----");
+            //printing out ips
+            for(String ip : instanceIPs) {
+                String ipNote = ip;
+                if(selfIP.equals(ip)) {
+                    ipNote += " (self)";
+                }
+                System.out.println(uniqueID + ": " + ipNote);
             }
-            System.out.println(uniqueID + ": " + ipNote);
+            System.out.println("/----");
         }
-        System.out.println("/----");
     }
-
 
     private static Set<String> determinePodIPs() {
         try {
             final String namespace = System.getenv("SELF_POD_NAMESPACE");
-
-            System.out.println(namespace);
 
             Set<String> instanceIPs = new HashSet<>();
 
@@ -60,7 +57,7 @@ public class KubernetesService {
                     final String appLabel = metadata.getLabels().getOrDefault("app", "none");
                     if ("hydra-proxy".equals(appLabel)) {
                         final V1PodStatus status = pod.getStatus();
-                        if (null != status) {
+                        if (null != status && status.getPodIP() != null) {
                             instanceIPs.add(status.getPodIP());
                         }
                     }
@@ -71,5 +68,4 @@ public class KubernetesService {
             throw new RuntimeException(e);
         }
     }
-
 }
